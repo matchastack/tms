75bf45bb6a2100249712ac91f587cc6672b3edbf
Zhan Yi <liauzhanyi@gmail.com>
2025-10-06 16:26:32 +0800

    chore: add Tailwind CSS and Vite dependencies to package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
         port: 3000
     },
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html'
+                main: "index.html"
             }
         }
     }
-})
+});
