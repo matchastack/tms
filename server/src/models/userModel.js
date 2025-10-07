958c746366c12ae2d00e546c92e51462dd5c41c5
Zhan Yi <liauzhanyi@gmail.com>
2025-10-07 14:32:35 +0800

    refactor: simplify function syntax and update user property in login response

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,6 +1,6 @@
 import { query } from "../config/database.js";
 
-export const findUserByName = async (name) => {
+export const findUserByName = async name => {
     const sql = "SELECT * FROM accounts WHERE username = ?";
     const results = await query(sql, [name]);
     return results[0];
