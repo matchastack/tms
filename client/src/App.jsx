09bf48ee51cd10b84db02055c5b4965993189fad
Zhan Yi <liauzhanyi@gmail.com>
2025-10-08 10:55:02 +0800

    feat: rename UsersManagement component to UsersManagementPage and update routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import LoginPage from "./LoginPage";
 import HomePage from "./HomePage";
-import UsersManagement from "./UsersManagement";
+import UsersManagementPage from "./UsersManagementPage";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -50,7 +50,7 @@ function App() {
                 <Route path="*" element={<Navigate to="/" replace />} />
                 <Route
                     path="/manage/accounts"
-                    element={<UsersManagement onLogout={handleLogout} />}
+                    element={<UsersManagementPage onLogout={handleLogout} />}
                 />
             </Routes>
         </BrowserRouter>
