8b5d4be9783794c82362d2a83c4af6591c7a5dfe
Zhan Yi <liauzhanyi@gmail.com>
2025-10-13 14:06:38 +0800

    feat: configure CORS to allow requests only from localhost:3000 for enhanced security

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import routes from "./routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
