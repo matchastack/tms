542310a0726ce52765b589da97c40c2b45c8d4ac
Zhan Yi <liauzhanyi@gmail.com>
2025-10-15 14:37:03 +0800

    feat: update accounts table schema and modify initial data insertion

--- database/init.sql
+++ database/init.sql
@@ -3,15 +3,12 @@ CREATE DATABASE IF NOT EXISTS nodelogin DEFAULT CHARACTER SET utf8 COLLATE utf8_
 USE nodelogin;
 
 CREATE TABLE IF NOT EXISTS accounts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+    username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    userGroup VARCHAR(50) NOT NULL,
+    userGroup LONGTEXT NOT NULL,
     isActive TINYINT(1) DEFAULT 1
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8; 
 
 INSERT INTO accounts (username, password, email, userGroup, isActive) VALUES
-("test", "test", "test@test.com", "dev", 1);
-INSERT INTO accounts (username, password, email, userGroup, isActive) VALUES
-("admin", "admin", "admin@m.com", "admin", 1);
+("admin", "$2a$10$xjP1qgUcVGXwCyu5OYEZFuGt5CsbjcK6oHFV.IJu4qyuXo62Dip46", "admin@m.com", "admin", 1);
