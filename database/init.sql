6193f23cd8a7fd60fce30c889509528801790c79
Zhan Yi <liauzhanyi@gmail.com>
2025-10-16 15:04:11 +0800

    feat: update accounts table to use userGroups JSON type for better group management

--- database/init.sql
+++ database/init.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS accounts (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    userGroup LONGTEXT NOT NULL,
+    userGroups JSON NOT NULL,
     isActive TINYINT(1) DEFAULT 1
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8; 
 
-INSERT INTO accounts (username, password, email, userGroup, isActive) VALUES
-("admin", "$2a$10$xjP1qgUcVGXwCyu5OYEZFuGt5CsbjcK6oHFV.IJu4qyuXo62Dip46", "admin@m.com", "admin", 1);
+INSERT INTO accounts (username, password, email, userGroups, isActive) VALUES
+("admin", "$2a$10$xjP1qgUcVGXwCyu5OYEZFuGt5CsbjcK6oHFV.IJu4qyuXo62Dip46", "admin@m.com", '["admin"]', 1);
