99a709897fd094af616924019899f3ef21447050
Zhan Yi <liauzhanyi@gmail.com>
2025-10-24 17:57:48 +0800

    refactor: remove unnecessary button for viewing tasks in AppPage

--- client/src/AppPage.jsx
+++ client/src/AppPage.jsx
@@ -510,29 +510,7 @@ const AppPage = () => {
                                                 >
                                                     Add
                                                 </button>
-                                            ) : (
-                                                <div className="flex gap-2 justify-center">
-                                                    <button
-                                                        onClick={() =>
-                                                            navigate(
-                                                                `/apps/${app.App_Acronym}/tasks`
-                                                            )
-                                                        }
-                                                        className="w-8 h-8 !bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors flex items-center justify-center"
-                                                        title="View Tasks"
-                                                    >
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            className="h-4 w-4"
-                                                            viewBox="0 0 20 20"
-                                                            fill="currentColor"
-                                                        >
-                                                            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                                                            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                            )}
+                                            ) : null}
                                         </td>
                                     </tr>
                                     {app.error && (
